1cd2c53e9f7834861ce112f3cb785dd90d4d5722
akimmariya <134380977+akimmariya@users.noreply.github.com>
2023-06-21 19:13:18 +0900

    Task 4.1 added

--- homeworkl_level4_task4.1.3.py
+++ homeworkl_level4_task4.1.3.py
@@ -8,12 +8,33 @@
 # Название школы:
 
 import sqlite3
-connection=sqlite3.connect('teachers.db')
-cursor=connection.cursor()
-query= '''JOIN School ON School.School_id=Students.School_id'''
-cursor.execute(query)
-connection.commit()
-connection.close()
 
-import sqlite3
+def get_connection():
+    connection =sqlite3.connect ('teachers.db')
+    return connection
+def close_connection(connection):
+    if connection:
+        connection.close()
 
+
+def get_student_id(student_id):
+    try:
+        connection = get_connection()
+        cursor= connection.cursor()
+        quary='''SELECT * from School
+        JOIN Students on School.school_id=Students.school_id where student_id=?
+       '''
+        cursor.execute(quary, (student_id,))
+        records= cursor.fetchall()
+        for row in records:
+            print ("Student_Id", row[0], )
+            print ("Student_Name", row[4], )
+            print ("School_id", row[3], )
+            print ("School_name", row[1], )
+                             
+        close_connection(connection)
+       
+    except (Exception, sqlite3.Error) as error:
+        print ('ошибка получения данных', error)
+
+get_student_id(202)
